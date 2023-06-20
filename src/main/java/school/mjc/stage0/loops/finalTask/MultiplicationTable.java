39c9d125a6a82374f78d6a5da102b41978582fa6
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-20 19:50:50 -0400

    Update MultiplicationTable.java

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,8 +2,9 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
-        for (int r = 0; r <= 10; r++) {
-            System.out.println(numberTableToPrint * r);
-        }
+   int t = 1;
+        for (int r = 1; r <= 10; r++) {
+            System.out.println( t+ " x " + numberTableToPrint + " = " +numberTableToPrint * r);
+       t++; }
     }
 }
