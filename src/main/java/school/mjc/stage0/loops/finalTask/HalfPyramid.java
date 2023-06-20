ca6a0dd905f66d3a5bbff81de6bb587de318df92
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-19 20:10:16 -0400

    Update HalfPyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+for (int r = cathetusLength; r > 0; r--) {
 
+            for (int c = 1; c < r; c++) {
+                System.out.print(" ");
+            }
+            for (int v = cathetusLength; v >= r; v--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
